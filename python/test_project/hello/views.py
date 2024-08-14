320ae4777ad84aa36ef0417b08ac467dd326ac39
Chioma Opara <93641892+ChiomaDevs@users.noreply.github.com>
2024-08-14 11:00:00 +0100

    new app

--- python/test_project/hello/views.py
+++ python/test_project/hello/views.py
@@ -3,4 +3,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world!")
+    return render(request, "hello/index.html")
+
+def Chioma(request):
+    return HttpResponse("Chioma is a good girl")
+
+def Obinna(request):
+    return HttpResponse("Obinna loves Chioma")
+
+def greet(request, name):
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
